e26d698300ccc973697f069f7f58faeee34bb6a5
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 14:43:31 -0800

    Update to new API

--- dropna.py
+++ dropna.py
@@ -1,25 +1,8 @@
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
+def render(table, params):
+    cols = params['colnames'].split(',')
+    cols = [c.strip() for c in cols]
+    if cols == [] or cols == ['']:
+        return table
 
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-        cols = wf_module.get_param_string('colnames').split(',')
-        cols = [c.strip() for c in cols]
-
-        if cols == [] or cols == ['']:
-            return table
-
-        for c in cols:
-            if not c in table.columns:
-                wf_module.set_error('There is no column named %s' % c)
-                return table
-
-        wf_module.set_ready(notify=False)
-        newtab = table.dropna(subset=cols, how='all', axis='index')
-        return newtab
+    newtab = table.dropna(subset=cols, how='all', axis='index')
+    return newtab
